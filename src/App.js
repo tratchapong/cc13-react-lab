c3bd03bba772d113991e9be5c88085e35baed11e
tratchapong <tratchapong@gmail.com>
2023-01-31 22:33:02 +0700

    Setup tailwind + google fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import SpecCard from "./SpecCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4 space-y-4">
+      <h1 className="text-3xl">Codecamp13 Project</h1>
+      <p className="text-3xl ">Personal project</p>
+      <SpecCard />
     </div>
   );
 }
