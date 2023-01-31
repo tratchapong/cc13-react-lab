26c0723524169a8514c3927f3b8155c75a6dff05
tratchapong <tratchapong@gmail.com>
2023-01-31 22:41:37 +0700

    correct class to className in SpecCard.jsx

--- src/SpecCard.jsx
+++ src/SpecCard.jsx
@@ -2,80 +2,80 @@ import React from "react";
 
 function SpecCard() {
   return (
-    <div class="flex font-sans shadow-md">
-      <div class="flex-none w-56 relative">
+    <div className="flex font-sans shadow-md max-w-2xl">
+      <div className="flex-none w-56 relative">
         <img
           src="https://picsum.photos/217/321"
           alt=""
-          class="absolute inset-0 w-full h-full object-cover rounded-lg"
+          className="absolute inset-0 w-full h-full object-cover rounded-lg"
           loading="lazy"
         />
       </div>
-      <form class="flex-auto p-6">
-        <div class="flex flex-wrap">
-          <h1 class="flex-auto font-medium text-slate-900">Kids Jumpsuit</h1>
-          <div class="w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600">
+      <form className="flex-auto p-6">
+        <div className="flex flex-wrap">
+          <h1 className="flex-auto font-medium text-slate-900">Kids Jumpsuit</h1>
+          <div className="w-full flex-none mt-2 order-1 text-3xl font-bold text-violet-600">
             $39.00
           </div>
-          <div class="text-sm font-medium text-slate-400">In stock</div>
+          <div className="text-sm font-medium text-slate-400">In stock</div>
         </div>
-        <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-          <div class="space-x-2 flex text-sm font-bold">
+        <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+          <div className="space-x-2 flex text-sm font-bold">
             <label>
               <input
-                class="sr-only peer"
+                className="sr-only peer"
                 name="size"
                 type="radio"
                 value="xs"
                 checked
               />
-              <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+              <div className="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
                 XS
               </div>
             </label>
             <label>
-              <input class="sr-only peer" name="size" type="radio" value="s" />
-              <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+              <input className="sr-only peer" name="size" type="radio" value="s" />
+              <div className="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
                 S
               </div>
             </label>
             <label>
-              <input class="sr-only peer" name="size" type="radio" value="m" />
-              <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+              <input className="sr-only peer" name="size" type="radio" value="m" />
+              <div className="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
                 M
               </div>
             </label>
             <label>
-              <input class="sr-only peer" name="size" type="radio" value="l" />
-              <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+              <input className="sr-only peer" name="size" type="radio" value="l" />
+              <div className="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
                 L
               </div>
             </label>
             <label>
-              <input class="sr-only peer" name="size" type="radio" value="xl" />
-              <div class="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
+              <input className="sr-only peer" name="size" type="radio" value="xl" />
+              <div className="w-9 h-9 rounded-full flex items-center justify-center text-violet-400 peer-checked:bg-violet-600 peer-checked:text-white">
                 XL
               </div>
             </label>
           </div>
         </div>
-        <div class="flex space-x-4 mb-5 text-sm font-medium">
-          <div class="flex-auto flex space-x-4">
+        <div className="flex space-x-4 mb-5 text-sm font-medium">
+          <div className="flex-auto flex space-x-4">
             <button
-              class="h-10 px-6 font-semibold rounded-full bg-violet-600 text-white"
+              className="h-10 px-6 font-semibold rounded-full bg-violet-600 text-white"
               type="submit"
             >
               Buy now
             </button>
             <button
-              class="h-10 px-6 font-semibold rounded-full border border-slate-200 text-slate-900"
+              className="h-10 px-6 font-semibold rounded-full border border-slate-200 text-slate-900"
               type="button"
             >
               Add to bag
             </button>
           </div>
           <button
-            class="flex-none flex items-center justify-center w-9 h-9 rounded-full text-violet-600 bg-violet-50"
+            className="flex-none flex items-center justify-center w-9 h-9 rounded-full text-violet-600 bg-violet-50"
             type="button"
             aria-label="Like"
           >
@@ -88,7 +88,7 @@ function SpecCard() {
             </svg>
           </button>
         </div>
-        <p class="text-sm text-slate-500">
+        <p className="text-sm text-slate-500">
           Free shipping on all continental US orders.
         </p>
       </form>
