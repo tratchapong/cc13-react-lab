d515130c719aa50eae96bbbf939e61869f5a7ae4
tratchapong <tratchapong@gmail.com>
2023-02-02 21:56:55 +0700

    add Header in LoginLayout

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 import Login from "../pages/Login";
 
 function LoginLayout() {
   return (
     <div className="flex flex-col h-screen">
+      <Header />
       <div className="flex-1 bg-violet-300">
         <Login />
       </div>
