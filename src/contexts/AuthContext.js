8f4b8a225c8d6205ba558bd7f3f471a3d022f6b6
tratchapong <tratchapong@gmail.com>
2023-02-07 17:37:55 +0700

    check authUser from token

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -5,32 +5,53 @@ import {createContext, useState, useContext, useEffect} from 'react'
 
 export const AuthContext = createContext()
 
+const URL = "http://localhost:8000";
+const putHeaders = (token) => ({
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
 export default function AuthContextProvider({children}) {
+  const [authUser, setAuthUser] = useState(null)
   const [isLogin, setIsLogin] = useState(false)
 
+  const getMe = () => {
+    let token = localStorage.getItem('token')
+    axios.get(`${URL}/me`, putHeaders(token)).then(res => {
+      setAuthUser(res.data)
+    })
+  }
+
   useEffect(() => {
-    if(localStorage.getItem('login')){
-      setIsLogin(true)
+    if(localStorage.getItem('token')){
+      getMe()
     }
   }, [])
   
+  useEffect(()=> {
+    setIsLogin(!!authUser)
+    console.log(authUser)
+  }, [authUser])
 
   const login = async (data) => {
     let res = await axios.post('http://localhost:8000/login',data)
     console.log(res.data)
     localStorage.setItem('token', res.data)
     localStorage.setItem('login', '1')
-    setIsLogin(true)
+    // setIsLogin(true)
+    getMe()
   }
 
   const logout = () => {
     localStorage.removeItem('login')
     localStorage.removeItem('token')
-    setIsLogin(false)
+    // setIsLogin(false)
+    setAuthUser(null)
   }
 
   return(
-    <AuthContext.Provider value={{isLogin, setIsLogin, login, logout}}>
+    <AuthContext.Provider value={{isLogin, setIsLogin, login, logout, authUser}}>
       {children}
     </AuthContext.Provider>
   )
